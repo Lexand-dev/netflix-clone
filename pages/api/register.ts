38805a0a68ccbf5fb30b3f228c17eae24b6c8d93
lexandfr <alexfloresrayme@gmail.com>
2023-11-24 19:12:32 -0500

    fix: fix auth

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -3,36 +3,37 @@ import bcrypt from 'bcrypt';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    res.status(405).json({ message: 'Method not allowed' });
-    return;
-  }
   try {
-    const { email, name,  password } = req.body;
+    if (req.method !== 'POST') {
+      return res.status(405).end();
+    }
+
+    const { email, name, password } = req.body;
 
     const existingUser = await prismadb.user.findUnique({
       where: {
-        email,
-      },
-    });
+        email
+      }
+    })
+
     if (existingUser) {
-      res.status(422).json({ message: 'User already exists' });
-      return;
+      return res.status(422).json({ error: 'Email taken' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
+
     const user = await prismadb.user.create({
       data: {
         email,
         name,
         hashedPassword,
-        image: "",
+        image: '',
         emailVerified: new Date(),
-      },
-    });
+      }
+    })
+
     return res.status(200).json(user);
-  }
-  catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
+  } catch (error) {
+    return res.status(400).json({ error: `Something went wrong: ${error}` });
   }
 }
